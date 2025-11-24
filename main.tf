1992b3875dac6f20782b67e3c508a644090a3a2f
Khushi Gupta <104582212+khushig99@users.noreply.github.com>
2025-11-24 15:16:50 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,25 @@
+resource "google_compute_instance" "jenkins_vm" {
+  name         = "jenkins-job-vm"
+  machine_type = var.machine_type
+  zone         = var.zone
+
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-12" # You can change to Ubuntu if needed
+    }
+  }
+
+  network_interface {
+    network = "default"
+
+    access_config {
+      # This enables external IP
+    }
+  }
+
+  tags = ["jenkins", "ci-cd"]
+
+  metadata = {
+    ssh-keys = "your-username:${file("~/.ssh/id_rsa.pub")}"
+  }
+}
